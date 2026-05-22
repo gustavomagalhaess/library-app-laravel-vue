69a149a8d7262f3e1227e3dcd4136e47132f40e1
Gustavo Santos <gustavo.magalhaess@gmail.com>
2026-05-22 10:13:06 -0300

    Updates layout.

--- library/resources/js/Pages/Dashboard.vue
+++ library/resources/js/Pages/Dashboard.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import SuccessButton from "@/Components/SuccessButton.vue";
 
 const props = defineProps({
     bookCount: { type: Number, required: true },
@@ -11,24 +10,24 @@ const props = defineProps({
 <template>
     <Head title="Dashboard" />
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-white/40">
-                Dashboard
-            </h2>
-        </template>
 
         <div class="py-8">
-            <div class="mx-auto container sm:px-6 lg:px-8">
-                <div class="max-w-64 rounded overflow-hidden shadow-lg">
-                    <div class="px-6 py-4 text-white bg-gray-700 opacity-85">
-                        <div class="font-bold text-xl mb-2 text-center">Total Books</div>
-                        <p class="text-base text-center">
-                            {{ bookCount }}
-                        </p>
+            <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="space-y-4">
+                    <div class="flex items-center justify-between gap-4">
+                        <h2 class="text-2xl font-semibold text-gray-800">Dashboard</h2>
+                    </div>
+                    <div class="max-w-64 rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4 text-white bg-gray-700 opacity-85">
+                            <div class="font-bold text-xl mb-2 text-center">Total Books</div>
+                            <p class="text-base text-center">
+                                {{ bookCount }}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
+
     </AuthenticatedLayout>
 </template>
